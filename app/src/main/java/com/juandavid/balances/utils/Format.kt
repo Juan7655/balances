f1c884079415a841b35e0392836f12ddec957432
Juan David <david.rodriguez@endava.com>
2020-02-09 20:27:32 -0500

    refactored format method

--- app/src/main/java/com/juandavid/balances/utils/Format.kt
+++ app/src/main/java/com/juandavid/balances/utils/Format.kt
@@ -1,6 +1,7 @@
 package com.juandavid.balances.utils
 
 import kotlin.math.absoluteValue
+import kotlin.math.pow
 
 object Format {
     fun currency(value: Int): String {
@@ -8,17 +9,24 @@ object Format {
         var numStr: String = absValue.toString()
         val sign = if (value >= 0) "$" else "$-"
 
-        if (absValue >= 1000000) {
-            numStr = (numStr.substring(0, numStr.length - 6)
-                    + "'"
-                    + numStr.substring(numStr.length - 6, numStr.length))
-        }
+        numStr = partialFormat(absValue, numStr, 6, "'")
+        numStr = partialFormat(absValue, numStr, 3, ",")
 
-        if (absValue >= 1000) {
-            numStr = (numStr.substring(0, numStr.length - 3)
-                    + ","
-                    + numStr.substring(numStr.length - 3, numStr.length))
-        }
         return sign + numStr
+    }
+
+    private fun partialFormat(
+        value: Int,
+        numStr: String,
+        position: Int,
+        separator: String
+    ): String {
+        return if (value >= 10.0.pow(position.toDouble())) {
+            (numStr.substring(0, numStr.length - position)
+                    + separator
+                    + numStr.substring(numStr.length - position, numStr.length))
+        } else {
+            numStr
+        }
     }
 }
